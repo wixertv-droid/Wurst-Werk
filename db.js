10768f7511182a989442f56019df1d6667f737c7
wixertv-droid <wixertv@googlemail.com>
2026-03-17 14:19:48 +0100

    db.js aktualisieren

--- db.js
+++ db.js
@@ -2,9 +2,8 @@ const supabaseUrl = 'https://lphunsmxoruyovvhjrxy.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwaHVuc214b3J1eW92dmhqcnh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3NDYxNDAsImV4cCI6MjA4OTMyMjE0MH0.LhW4nqr98xAU-0eun-qTByJBxBjRk767CTzVCtB0eEI';
 
 const db = {
-    // Funktion zum Daten abrufen (Lagerbestand)
     getInventory: async function() {
-        const response = await fetch(`${supabaseUrl}/rest/v1/inventory?select=*`, {
+        const response = await fetch(`${supabaseUrl}/rest/v1/inventory?select=*&order=name.asc`, {
             headers: {
                 'apikey': supabaseKey,
                 'Authorization': `Bearer ${supabaseKey}`
@@ -13,15 +12,13 @@ const db = {
         return await response.json();
     },
 
-    // Funktion zum Aktualisieren (z.B. nach Einkauf oder Produktion)
     updateStock: async function(id, newAmount) {
         await fetch(`${supabaseUrl}/rest/v1/inventory?id=eq.${id}`, {
             method: 'PATCH',
             headers: {
                 'apikey': supabaseKey,
                 'Authorization': `Bearer ${supabaseKey}`,
-                'Content-Type': 'application/json',
-                'Prefer': 'return=minimal'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({ amount: newAmount })
         });
